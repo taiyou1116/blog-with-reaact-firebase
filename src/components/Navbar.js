169fb08bb0f1d45403939ec1019bcbc0823bc6eb
taiyou <hiromacha1116@icloud.com>
2023-08-19 21:52:07 +0900

    change login logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,20 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import "./Navbar.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faFilePen, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faFilePen, faArrowRightToBracket, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
 
-
-function Navbar() {
+function Navbar({ isAuth }) {
+  console.log(isAuth);
   return (
     <nav>
       <Link to={"/"}><FontAwesomeIcon icon={faHouse} />ホーム</Link>
       <Link to={"/createpost"}><FontAwesomeIcon icon={faFilePen} />記事投稿</Link>
-      {  }
-      <Link to={"/login"}><FontAwesomeIcon icon={faArrowRightToBracket} />ログイン</Link>
+      { !isAuth ?  
+        <Link to={"/login"}><FontAwesomeIcon icon={faArrowRightToBracket} />ログイン</Link>
+        :
+        <Link to={"/logout"}><FontAwesomeIcon icon={faArrowRightFromBracket} />ログアウト</Link>
+      }
     </nav>
   )
 }
